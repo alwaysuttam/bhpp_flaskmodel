112a0f3a6dcfd40dc174104c2d592ee69c985972
Alwaysuttam <itsuttammail@gmail.com>
2024-05-14 15:09:18 +0530

    flask model updated

--- server.py
+++ server.py
@@ -1,59 +1,50 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template, jsonify
 import json
-import pandas as pd
 import numpy as np
 import pickle
 
 app = Flask(__name__)
 
-global __location
-global __model
-global __data_column
-
-__data_column = None
-__location = {}
-
-# Load model and location data during startup
+# Load data during startup
 with open("data/columns.json", 'r') as f:
-    __data_column = json.load(f)['data_columns']
-    __location = {location.lower(): index for index, location in enumerate(__data_column[3:])}
+    data_columns = json.load(f)['data_columns']
+    locations = data_columns[3:]
 
-with open("data/banglore_9.pickle", 'rb') as f:
+with open("data/bhpp.pickle", 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', locations=locations)
 
 @app.route('/predict', methods=['POST'])
 def estimate_price():
     data = request.form
     location = data['location']
-    print('Location: ',location)
+    print("1st print",location)
     sqft = float(data['sqft'])
-    print('Sqft: ',sqft)
     bhk = int(data['bhk'])
-    print('BHK: ',bhk)
     bath = int(data['bath'])
-    print('Bath: ',bath)
 
     # Check if location exists in the location_data dictionary
-    if location.lower() not in __location:
+    if location not in locations:
+        print("2nd one",location)
+        print("locations",locations)
         return jsonify({'error': 'Invalid location'}), 400
 
-    loc_index = __location[location.lower()]
+    loc_index = locations.index(location)
 
-    input_features = np.zeros(len(__data_column))
+    input_features = np.zeros(len(data_columns))
     input_features[0] = sqft
     input_features[1] = bath
     input_features[2] = bhk
     if loc_index >= 0:
         input_features[loc_index + 3] = 1
 
-    estimated_price = round(model.predict([input_features])[0], 2)
-    print("Estimated price", estimated_price)
+    predicted_price = round(model.predict([input_features])[0], 2)
+    print("Estimated price", predicted_price)
     # Pass the estimated price directly to the template
-    return render_template('predict.html', estimated_price=estimated_price)
+    return render_template('index.html', locations=locations, predicted_price=predicted_price)
 
 if __name__ == '__main__':
     print("Starting")
